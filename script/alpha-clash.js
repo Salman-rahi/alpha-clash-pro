93f0520703dade42a270f3e49eae45f857df499b
Salman-rahi <95650802+Salman-rahi@users.noreply.github.com>
2024-08-29 08:36:11 -0400

    update score

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -12,9 +12,47 @@
 
  } */
 
+function haddlekeyBoardKeyUpEvent(event){
+    const playerPress =event.key;
+    // console.log('player pressed ',playerPress);
+    // get the expected press
+    const currentAlphabetElement = document.getElementById('current-alpahbet');
+    
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expecteAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPress,expecteAlphabet);
+
+    // chcek
+    if(playerPress === expecteAlphabet){
+        console.log('you get a point');
+        // update score
+        const currentScoreElement =document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        console.log(currentScore);
+
+        // new score 
+        const newScore = currentScore + 1;
+
+        // show update score
+
+        currentScoreElement.innerText = newScore;
+        // start new round
+        removeBackgroundColorById(expecteAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('you lose point');
+    }
+}
+
+document.addEventListener('keyup',haddlekeyBoardKeyUpEvent);
+    
+
+
 function continueGame(){
     const alphabet = getRandonmAlphabet();
-    console.log('Your random alphaber',alphabet);
+    // console.log('Your random alphaber',alphabet);
 
     // set randomely generated alpahbet to the screen(display it)
     const currentAlphabetElement = document.getElementById('current-alpahbet');
